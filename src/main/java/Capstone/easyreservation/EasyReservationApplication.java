0d0e126c7b65fef67a6ec63e7890751570fac328
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-09-04 19:37:29 +0200

    Create project

---
+++ src/main/java/Capstone/easyreservation/EasyReservationApplication.java
@@ -0,0 +1,13 @@
+package Capstone.easyreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Capstone/easyreservation/EasyReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.easyreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
