0b202016a25bbaca3257b77bb2091628190d94e8
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-09-06 14:13:36 +0200

    aggiornamento crud hotel

--- src/main/java/Capstone/easyreservation/repository/HotelRepository.java
+++ src/main/java/Capstone/easyreservation/repository/HotelRepository.java
@@ -1,5 +1,7 @@
 package Capstone.easyreservation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,8 @@ import Capstone.easyreservation.entity.Hotel;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
+	List<Hotel> findByNomeContainingIgnoreCase(String nome);
+
+
+
 }
